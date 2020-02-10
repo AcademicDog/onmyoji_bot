8e7427e81578750714be697124e2df53f552fcca
YangHu <chaos.huyang@gmail.com>
2020-02-10 16:11:58 +0800

    1.0.1.0210

--- onmyoji.py
+++ onmyoji.py
@@ -1,58 +1,55 @@
-import sys
-import os
-import ctypes
-
 from explore.explore import ExploreFight
+from goryou.single_fight import GoryouFight
+from mitama.dual import DualFighter
 from mitama.fighter_driver import DriverFighter
 from mitama.fighter_passenger import FighterPassenger
 from mitama.single_fight import SingleFight
-from tools.logsystem import WriteLog
 
-# 设置
-global mode
-global emyc
-global done
+import configparser
+import ctypes
+import logging
+import os
+import sys
+import tools.logsystem
 
-#初始化对象
-log = WriteLog()
 
 def init():
-    global section
-    global mode
-    global emyc
-    global done
-    
-    try:
-        # 选择打什么
-        section = int(input('\n选择刷什么(Ctrl-C跳过并单刷御魂：\n0-御魂\n1-探索\n'))
-        log.writeinfo('Section = %d', section)
-        if section == 0:
-            # 御魂模式选择
-            mode=int(input('\n选择游戏模式(Ctrl-C跳过并单刷)：\n0-单刷\n2-组队司机\n3-组队打手\n'))
-            if(mode==1):
-                log.writewarning('未开发，告辞！')
-                os._exit(0)
-            elif(mode != 2 and mode != 0 and mode != 3):
-                mode=0
-        
-        # 点怪设置
-        # emyc=int(input('\n是否点怪？\n0-不点怪\n1-点中间怪\n2-点右边怪\n'))
-        # if((emyc!=0) and (emyc!=1) and (emyc!=2)):
-        #     emyc=0
-        
-        # 结束设置
-        # done=int(input('\n结束后如何处理？\n0-退出\n1-关机\n'))
-        # if not ((done == 0) or (done == 1)):
-        #     done = 0
-            log.writeinfo('Mode = %d',mode)
-        # log.writeinfo('Emyc = %d',emyc)
-        # log.writeinfo('Postoperation = %d',done)
-    except:
-        section = 0
-        mode=0
-        emyc=0
-        done=1
-        log.writeinfo('Use default parameters')
+    conf = configparser.ConfigParser()
+    # 读取配置文件
+    conf.read('conf.ini', encoding="utf-8")
+
+    # 读取主要副本
+    section = conf.getint('DEFAULT', 'run_section')
+
+    if section == 0:
+        # 御魂
+        mode = conf.getint('DEFAULT', 'run_mode')
+        if mode == 0:
+            # 单刷
+            fight = SingleFight()
+
+        elif mode == 1:
+            # 司机
+            fight = DriverFighter()
+
+        elif mode == 2:
+            # 乘客
+            fight = FighterPassenger()
+
+        elif mode == 3:
+            # 双开
+            fight = DualFighter()
+
+    elif section == 1:
+        # 御灵
+        fight = GoryouFight()
+
+    elif section == 2:
+        # 探索
+        fight = ExploreFight()
+
+    fight.start()
+
 
 def is_admin():
     # UAC申请，获得管理员权限
@@ -61,50 +58,39 @@ def is_admin():
     except:
         return False
 
-def yuhun():
-    '''御魂战斗'''
-    if mode == 0:
-        # 单刷
-        fight = SingleFight()
-        fight.start()
-    
-    if mode == 2:
-        # 司机
-        fight = DriverFighter()
-        fight.start()
-    
-    if mode == 3:
-        # 乘客
-        fight = FighterPassenger()        
-        fight.start()    
-    
-def tansuo():
-    '''探索战斗'''
-    fight = ExploreFight()
-    fight.start()
+def my_excepthook(exc_type, exc_value, tb):
+    msg = ' Traceback (most recent call last):\n'
+    while tb:
+        filename = tb.tb_frame.f_code.co_filename
+        name = tb.tb_frame.f_code.co_name
+        lineno = tb.tb_lineno
+        msg += '   File "%.500s", line %d, in %.500s\n' % (
+            filename, lineno, name)
+        tb = tb.tb_next
 
-if __name__ == "__main__":    
-    log.writeinfo('python version: %s', sys.version)
-    
+    msg += ' %s: %s\n' % (exc_type.__name__, exc_value)
+
+    logging.error(msg)
+
+
+if __name__ == "__main__":
+    logging.info('python version: %s', sys.version)
+
     try:
         # 检测管理员权限
         if is_admin():
             # 注册插件，获取权限
-            log.writeinfo('UAC pass')            
+            sys.excepthook = my_excepthook
+            logging.info('UAC pass')
 
             # 设置战斗参数
             init()
 
-            # 开始战斗
-            if section == 0:
-                yuhun()
-            elif section == 1:
-                tansuo()
-
         else:
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)       
-    except KeyboardInterrupt:        
-        log.writeinfo('terminated')
+            ctypes.windll.shell32.ShellExecuteW(
+                None, "runas", sys.executable, __file__, None, 1)
+    except KeyboardInterrupt:
+        logging.info('terminated')
         os._exit(0)
     else:
         pass
--- ui.py
+++ ui.py
@@ -1,20 +1,13 @@
-from explore.explore import ExploreFight
-from goryou.single_fight import GoryouFight
-from mitama.dual import DualFighter
-from mitama.fighter_driver import DriverFighter
-from mitama.fighter_passenger import FighterPassenger
-from mitama.single_fight import SingleFight
-from PyQt5.QtGui import QTextCursor
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QObject, pyqtSignal
 from Ui_onmyoji import Ui_MainWindow
 
 import configparser
 import ctypes
 import logging
 import os
+import subprocess
 import sys
-import threading
+import tools.logsystem
 
 
 def is_admin():
@@ -25,113 +18,83 @@ def is_admin():
         return False
 
 
-class GuiLogger(logging.Handler):
-    def emit(self, record):
-        # implementation of append_line omitted
-        self.edit.append(self.format(record))
-        self.edit.moveCursor(QTextCursor.End)
-
-
 class MyMainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.conf = configparser.ConfigParser()
 
-        self.ui.textEdit.ensureCursorVisible()
-
-        h = GuiLogger()
-        h.edit = self.ui.textEdit
-        logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
-        logger.addHandler(h)
-
-    def set_conf(self, conf, section):
+    def set_conf(self):
         '''
         设置参数至配置文件
         '''
-        # 一般参数
-        conf.set('watchdog', 'watchdog_enable',
-                 str(self.ui.checkBox.isChecked()))
-        conf.set('watchdog', 'max_win_time', str(self.ui.lineEdit.text()))
-        conf.set('watchdog', 'max_op_time', str(self.ui.lineEdit_2.text()))
+        # 运行参数
+        section = self.ui.tabWidget.currentIndex()
+        self.conf.set('DEFAULT', 'run_section', str(section))
 
-        conf.set('others', 'debug_enable', str(self.ui.checkBox_4.isChecked()))
+        # 一般参数
+        self.conf.set('watchdog', 'watchdog_enable',
+                      str(self.ui.checkBox.isChecked()))
+        self.conf.set('watchdog', 'max_win_time', str(self.ui.lineEdit.text()))
+        self.conf.set('watchdog', 'max_op_time',
+                      str(self.ui.lineEdit_2.text()))
+
+        self.conf.set('others', 'debug_enable', str(
+            self.ui.checkBox_4.isChecked()))
 
         # 御魂参数
-        if section == 0:
-            pass
+        if self.ui.mitama_single.isChecked():
+            # 单刷
+            self.conf.set('DEFAULT', 'run_mode', '0')
+
+        elif self.ui.mitama_driver.isChecked():
+            # 司机
+            self.conf.set('DEFAULT', 'run_mode', '1')
+
+        elif self.ui.mitama_passenger.isChecked():
+            # 乘客
+            self.conf.set('DEFAULT', 'run_mode', '2')
+
+        elif self.ui.mitama_dual.isChecked():
+            # 双开
+            self.conf.set('DEFAULT', 'run_mode', '3')
 
         # 探索参数
-        if section == 1:
-            # 探索
-            conf.set('explore', 'fight_boss_enable',
-                     str(self.ui.checkBox_2.isChecked()))
-            conf.set('explore', 'slide_shikigami',
-                     str(self.ui.checkBox_3.isChecked()))
-            conf.set('explore', 'slide_shikigami_progress',
-                     str(self.ui.horizontalSlider.value()))
-            conf.set('explore', 'zhunbei_delay',
-                     str(self.ui.lineEdit_3.text()))
+        self.conf.set('explore', 'fight_boss_enable',
+                      str(self.ui.checkBox_2.isChecked()))
+        self.conf.set('explore', 'slide_shikigami',
+                      str(self.ui.checkBox_3.isChecked()))
+        self.conf.set('explore', 'slide_shikigami_progress',
+                      str(self.ui.horizontalSlider.value()))
+        self.conf.set('explore', 'zhunbei_delay',
+                      str(self.ui.lineEdit_3.text()))
 
-    def get_conf(self, section):
-        conf = configparser.ConfigParser()
-        # 读取配置文件
-        conf.read('conf.ini', encoding="utf-8")
+    def get_conf(self):
+        # 添加配置
+        try:
+            self.conf.add_section('watchdog')
+            self.conf.add_section('explore')
+            self.conf.add_section('others')
+        except:
+            pass
 
         # 修改配置
-        try:
-            self.set_conf(conf, section)
-        except:
-            conf.add_section('watchdog')
-            conf.add_section('explore')
-            conf.add_section('others')
-            self.set_conf(conf, section)
+        self.set_conf()
 
         # 保存配置文件
         with open('conf.ini', 'w') as configfile:
-            conf.write(configfile)
+            self.conf.write(configfile)
 
     def start_onmyoji(self):
-        section = self.ui.tabWidget.currentIndex()
-
         # 读取主要副本
-        self.get_conf(section)
+        self.get_conf()
 
-        if section == 0:
-            # 御魂
-            if self.ui.mitama_single.isChecked():
-                # 单刷
-                self.fight = SingleFight()
-
-            elif self.ui.mitama_driver.isChecked():
-                # 司机
-                self.fight = DriverFighter()
-
-            if self.ui.mitama_passenger.isChecked():
-                # 乘客
-                self.fight = FighterPassenger()
-
-            if self.ui.mitama_dual.isChecked():
-                # 双开
-                self.fight = DualFighter()
-
-        elif section == 1:
-            # 御灵
-            self.fight = GoryouFight()
-
-        elif section == 2:
-            # 探索
-            self.fight = ExploreFight()
-
-        task = threading.Thread(target=self.fight.start)
-        task.start()
+        subprocess.Popen("cmd.exe /c start onmyoji.exe")
+        #os.system("onmyoji.exe")
 
     def stop_onmyoji(self):
-        try:
-            self.fight.deactivate()
-        except:
-            pass
+        os._exit(0)
 
 
 def my_excepthook(exc_type, exc_value, tb):
@@ -140,10 +103,11 @@ def my_excepthook(exc_type, exc_value, tb):
         filename = tb.tb_frame.f_code.co_filename
         name = tb.tb_frame.f_code.co_name
         lineno = tb.tb_lineno
-        msg += '   File "%.500s", line %d, in %.500s\n' % (filename, lineno, name)
+        msg += '   File "%.500s", line %d, in %.500s\n' % (
+            filename, lineno, name)
         tb = tb.tb_next
 
-    msg += ' %s: %s\n' %(exc_type.__name__, exc_value)
+    msg += ' %s: %s\n' % (exc_type.__name__, exc_value)
 
     logging.error(msg)
 
@@ -154,10 +118,6 @@ if __name__ == "__main__":
         # 检测管理员权限
         if is_admin():
             sys.excepthook = my_excepthook
-            global mode
-            mode = 0
-            global section
-            section = 0
 
             # 设置战斗参数
             app = QApplication(sys.argv)
